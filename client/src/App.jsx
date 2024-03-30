38c64a0b16931378ee537a98361d3289ede063ad
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-03-30 15:00:33 +0530

    completed the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Header from './components/Header'
+import Foter from "./components/Foter"
+import PrivateRoute from './components/PrivateRoutes';
 
-
-function App() {
+export default function App() {
   return (
-    <div className="text-red-500">App</div>
-  )
-}
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element = {<About />}/>
+        <Route path='/projects' element = {<Projects/>}/>
+        <Route element = {<PrivateRoute/>}>
+          <Route path='/dashboard' element = {<Dashboard/>}/>
+        </Route>
 
-export default App
+        <Route path='/signup' element = {<Signup/>}/>
+        <Route path='/signin' element = {<Signin/>}/>
+      </Routes>
+      <Foter/>
+    </BrowserRouter>
+  );
+}
